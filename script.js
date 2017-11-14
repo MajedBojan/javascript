dbcdb7f5d6a2a63803daed1962093564f03e8c01
ClickApps <bojanmajed@gmail.com>
2017-11-14 18:37:27 +0530

    start if condations

---
+++ script.js
@@ -0,0 +1,13 @@
+var date = new Date();
+document.body.innerHTML = '<h1> Today is: ' + date + '</h1>';
+
+var a = 5, b = 5, theNumberMatch;
+
+if (a == b) {
+  theNumberMatch = true;
+} else {
+  theNumberMatch = false
+}
+
+console.log('the result: ' + theNumberMatch)
+// document.body.innerHTML = '<h1> the result: ' + theNumberMatch + '</h1>';
